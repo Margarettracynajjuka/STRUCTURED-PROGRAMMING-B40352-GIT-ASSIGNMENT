fe0c4046ffc8c1973baae6f055c61afbd7775321
Margarettracynajjuka <tracynajjuka@gmail.com>
2026-09-25 13:47:16 +0300

    Create chapter2-excercise2.4.c
    added my number 2 of c program with scan(f) and variables

---
+++ INPUT-OUTPUT-PROCESS/chapter2-excercise2.4.c
@@ -0,0 +1,21 @@
+//najjukamargarettracynumber2
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x,y,z,result;
+    printf("Enter three integers: x\n");
+    scanf("%d",&x);
+
+    printf("Enter three integers: y\n");
+    scanf("%d",&y);
+
+    printf("Enter three integers: z\n");
+    scanf("%d",&z);
+
+    result=x*y*z;
+
+    printf("The product is %d\n", result);
+    return 0;
+}
