b28c8c2ee5c1a69351ae7286736c0a36c73179a1
Margarettracynajjuka <tracynajjuka@gmail.com>
2026-09-26 08:04:30 +0300

    Create chapter3-exercise3.3.c
    added number 3

---
+++ DECISIONS/chapter3-exercise3.3.c
@@ -0,0 +1,17 @@
+//najjukamargarettracynumber3
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int product=5, count=15;
+
+    product*=2;
+    product=product*2;
+
+    if (count>10){
+        printf("count is greater than 10\n ");
+    }
+
+    return 0;
+}
